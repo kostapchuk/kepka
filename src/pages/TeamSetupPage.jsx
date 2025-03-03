222a7285e5c946eeff9f40fed7091f5886d8ff0a
kostapchuk <xfaseex1@gmail.com>
2025-03-03 16:24:31 +0300

    fix: allow any number of players

--- src/pages/TeamSetupPage.jsx
+++ src/pages/TeamSetupPage.jsx
@@ -1,5 +1,5 @@
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {addPlayers} from "../redux/playersSlice";
 import {setCurrentPage} from "../redux/pageSlice";
 import {Pages} from "../routes";
@@ -12,63 +12,84 @@ import Footer from "../components/Footer";
 import {randomIndex} from "../util/arrayUtils";
 
 const TeamSetupPage = () => {
-  const dispatch = useDispatch();
-  const [playerNames, setPlayerNames] = useState(['', '', '']);
-  const [gameId, setGameId] = useState('');
-  const players = useSelector(state => state.players);
+    const dispatch = useDispatch();
+    const [playerNames, setPlayerNames] = useState([]);
+    const [gameId, setGameId] = useState('');
+    const players = useSelector(state => state.players);
+    const [playerName, setPlayerName] = useState('');
 
-  useEffect(() => {
-    const newGameId = uuidv4();
-    setGameId(newGameId);
-  }, []);
+    useEffect(() => {
+        const newGameId = uuidv4();
+        setGameId(newGameId);
+    }, []);
 
-  const handleInputChange = (index, value) => {
-    const newPlayerNames = [...playerNames];
-    newPlayerNames[index] = value;
-    setPlayerNames(newPlayerNames);
-  };
 
-  const addPlayersToRedux = () => {
-    const filledPlayerNames = playerNames.filter(name => name.trim() !== '')
-    const teamId = playerNames.join(' ');
-    const askerIndex = randomIndex(filledPlayerNames);
-    const newPlayers = filledPlayerNames
-        .map((name, index) => ({
-          name: name,
-          asker: index === askerIndex,
-          teamId: teamId,
-          gameId: gameId,
-        }));
-    dispatch(addPlayers(newPlayers));
-    setPlayerNames(['', '', '']);
-  };
+    const handlePlayerNameChange = (name) => {
+        setPlayerName(name);
+    };
 
-  const goToNextPage = () => {
-    dispatch(setCurrentGameId(gameId))
-    dispatch(setCurrentPage(Pages.GAME_SETUP_PAGE));
-  }
+    const handleBlur = () => {
+        setPlayerNames(prevState => [...prevState, playerName]);
+        setPlayerName('');
+    }
 
-  return (
-      <Stack spacing={2}>
-        {playerNames.map((name, index) => (
+    const handlePlayerNameChangeByIndex = (index, name) => {
+        const updatedNames = [...playerNames];
+        updatedNames[index] = name;
+        setPlayerNames(updatedNames);
+    };
+
+    const addPlayersToRedux = () => {
+        const filledPlayerNames = playerNames.filter(name => name.trim() !== '')
+        const teamId = playerNames.join(' ');
+        const askerIndex = randomIndex(filledPlayerNames);
+        const newPlayers = filledPlayerNames
+            .map((name, index) => ({
+                name: name,
+                asker: index === askerIndex,
+                teamId: teamId,
+                gameId: gameId
+            }));
+        dispatch(addPlayers(newPlayers));
+        setPlayerNames([]);
+    };
+
+    const goToNextPage = () => {
+        dispatch(setCurrentGameId(gameId))
+        dispatch(setCurrentPage(Pages.GAME_SETUP_PAGE));
+    }
+
+    return (
+        <Stack spacing={2}>
+            {playerNames.map((name, index) => (
+                <TextField
+                    key={index}
+                    value={name}
+                    onChange={(e) => handlePlayerNameChangeByIndex(index, e.target.value)}
+                    variant="outlined"
+                    fullWidth
+                />
+            ))}
             <TextField
-                key={index}
-                label={`Имя игрока ${index + 1}`}
-                value={name}
-                onChange={(e) => handleInputChange(index, e.target.value)}
+                key='emptyPlayerInputKey'
+                label="Введите имя игрока"
+                value={playerName}
+                onChange={(e) => handlePlayerNameChange(e.target.value)}
                 variant="outlined"
+                onBlur={handleBlur}
                 fullWidth
             />
-        ))}
-        <Button variant="contained" onClick={addPlayersToRedux} disabled={playerNames.filter(name => name.trim() !== '').length < 2 || playerNames.length !== new Set(playerNames).size}>
-          Добавить игроков
-        </Button>
-        <Button variant="contained" onClick={goToNextPage} disabled={new Set(players.filter(p => p.gameId === gameId).map(p => p.teamId)).size < 2}>
-          Перейти к настройке игровой команты
-        </Button>
-        <Footer/>
-      </Stack>
-  );
+            <Button variant="contained" onClick={addPlayersToRedux}
+                    disabled={playerNames.filter(name => name.trim() !== '').length < 2 || playerNames.length !== new Set(playerNames).size}>
+                Добавить игроков
+            </Button>
+            <Button variant="contained" onClick={goToNextPage}
+                    disabled={new Set(players.filter(p => p.gameId === gameId).map(p => p.teamId)).size < 2}>
+                Продолжить
+            </Button>
+            <Footer/>
+        </Stack>
+    );
 };
 
 export default TeamSetupPage;
