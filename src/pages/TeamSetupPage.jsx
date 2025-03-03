82d6b18b32b7f68d557899c96bc32c6e22b17c12
kostapchuk <xfaseex1@gmail.com>
2025-03-03 18:54:55 +0300

    feature: delete team/player

--- src/pages/TeamSetupPage.jsx
+++ src/pages/TeamSetupPage.jsx
@@ -4,14 +4,14 @@ import {setCurrentPage} from "../redux/pageSlice";
 import {Pages} from "../routes";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import {TextField} from "@mui/material";
+import {Box, InputAdornment, TextField} from "@mui/material";
 import Footer from "../components/Footer";
 import {addPlayers} from "../redux/playersSlice";
 import {randomIndex} from "../util/arrayUtils";
 
-// todo: delete team/player
 // todo: design
 // todo: save to redux
+// todo: prohibit empty inputs && same player names
 const TeamSetupPage = () => {
     const dispatch = useDispatch();
     const {currentGameId} = useSelector(state => state.game)
@@ -108,26 +108,66 @@ const TeamSetupPage = () => {
         return true;
     }
 
+    const handleDeleteTeam = (teamIndexToDelete) => {
+        setTeams([...teams].filter((team, teamIndex) => teamIndex !== teamIndexToDelete))
+    }
+
+    const handleDeletePlayer = (teamIndex, playerIndexToDelete) => {
+        const updatedTeams = [...teams]
+        const updatedTeam = {
+            ...teams[teamIndex],
+            players: teams[teamIndex].players.filter((player, playerIndex) => playerIndex !== playerIndexToDelete)
+        }
+        updatedTeams[teamIndex] = updatedTeam
+        setTeams(updatedTeams)
+    }
+
     return (
         <Stack spacing={2}>
             {teams.map((team, teamIndex) => (
                 <>
                     <h3>Команда</h3>
-                    <TextField
-                        key={teamIndex}
-                        value={team.name}
-                        onChange={(e) => handleTeamNameChangeByIndex(teamIndex, e.target.value)}
-                        variant="outlined"
-                        fullWidth
-                    />
-                    {team.players.map((player, playerIndex) => (
+                    <Box sx={{display: 'flex', alignItems: 'center'}}>
                         <TextField
-                            key={teamIndex + playerIndex}
-                            value={player}
-                            onChange={(e) => handlePlayerNameChangeByIndex(playerIndex, teamIndex, e.target.value)}
+                            sx={{backgroundColor: '#F6F5F8'}}
+                            key={teamIndex}
+                            value={team.name}
+                            onChange={(e) => handleTeamNameChangeByIndex(teamIndex, e.target.value)}
                             variant="outlined"
                             fullWidth
+                            slotProps={{
+                                input: {
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <img src="/cap.svg" alt="Cap"/>
+                                        </InputAdornment>
+                                    )
+                                }
+                            }}
                         />
+                        <img
+                            src="/close.svg"
+                            alt="Delete team"
+                            onClick={() => handleDeleteTeam(teamIndex)}
+                            style={{cursor: 'pointer', marginLeft: '8px'}}
+                        />
+                    </Box>
+                    {team.players.map((player, playerIndex) => (
+                        <Box sx={{display: 'flex', alignItems: 'center'}}>
+                            <TextField
+                                key={teamIndex + playerIndex}
+                                value={player}
+                                onChange={(e) => handlePlayerNameChangeByIndex(playerIndex, teamIndex, e.target.value)}
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <img
+                                src="/close.svg"
+                                alt="Delete player"
+                                onClick={() => handleDeletePlayer(teamIndex, playerIndex)}
+                                style={{cursor: 'pointer', marginLeft: '8px'}}
+                            />
+                        </Box>
                     ))}
                     <TextField
                         key={teamIndex + 'emptyPlayerInputKey'}
@@ -142,6 +182,7 @@ const TeamSetupPage = () => {
             ))}
             <h3>Новая Команда</h3>
             <TextField
+                sx={{backgroundColor: '#F6F5F8'}}
                 key="emptyTeamInputKey"
                 label="Введите имя новой команды"
                 value={teamName}
@@ -149,6 +190,15 @@ const TeamSetupPage = () => {
                 variant="outlined"
                 onBlur={handleTeamBlur}
                 fullWidth
+                slotProps={{
+                    input: {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <img src="/cap.svg" alt="Cap"/>
+                            </InputAdornment>
+                        )
+                    }
+                }}
             />
             <Button variant="contained" onClick={goToNextPage} disabled={!areTeamsValid()}>
                 Продолжить
