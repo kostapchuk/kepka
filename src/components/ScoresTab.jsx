aad49af22616dc848bb296cbf624eddede1d7e06
kostapchuk <xfaseex1@gmail.com>
2025-03-16 23:45:31 +0300

    fix: score showing on results page

--- src/components/ScoresTab.jsx
+++ src/components/ScoresTab.jsx
@@ -2,19 +2,22 @@ import {useSelector} from "react-redux";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import React from 'react';
+import {Pages} from "../routes";
 
 const ScoresTab = () => {
 
     const {score, showScoreDuringGame} = useSelector(state => state.game);
+    const currentPage = useSelector(state => state.page.currentPage);
 
     return (
         <>
-            {showScoreDuringGame && Object.entries(score).map(([key, value]) => (
+            {Object.entries(score).map(([key, value]) => (
                 <Box sx={{display: 'flex', marginBottom: '20px'}}>
                     <img src="/cap.png" alt="Cap" style={{width: '50px', marginRight: '12px'}}/>
                     <Box>
                         <Typography sx={{fontSize: '16px'}}>{key}</Typography>
-                        {showScoreDuringGame && <Typography sx={{fontSize: '14px', color: '#6B6B6B'}}>{value}</Typography>}
+                        {(showScoreDuringGame || currentPage === Pages.RESULTS_PAGE) &&
+                            <Typography sx={{fontSize: '14px', color: '#6B6B6B'}}>{value}</Typography>}
                     </Box>
                 </Box>
             ))}
