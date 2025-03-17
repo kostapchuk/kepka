51921927d1757e7ff64d1976781cd0cdaafec193
kostapchuk <xfaseex1@gmail.com>
2025-03-17 17:29:03 +0300

    refactor: game page code improvements

--- src/components/AlarmTimer.jsx
+++ src/components/AlarmTimer.jsx
@@ -1,16 +1,28 @@
 import {useEffect, useRef} from "react";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import React from 'react';
+import {setRoundInProgress, setTimerRunning} from "../redux/gameSlice";
+import {setCurrentPage} from "../redux/pageSlice";
+import {Pages} from "../routes";
 
-const AlarmTimer = ({onTimerEnd}) => {
+const AlarmTimer = () => {
+
+  const dispatch = useDispatch();
 
   const audioRef = useRef(new Audio('/alarm-bell.mp3'));
+
   const {
     leftSeconds,
     currentTeam,
     timerRunning,
     timer: roundDuration
   } = useSelector(state => state.game);
+
+  const onRoundFinished = () => {
+    dispatch(setTimerRunning(false));
+    dispatch(setCurrentPage(Pages.ROUND_SCORE_PAGE));
+    dispatch(setRoundInProgress(false));
+  };
 
   useEffect(() => {
     if (timerRunning) {
@@ -19,7 +31,7 @@ const AlarmTimer = ({onTimerEnd}) => {
         const audioPauseTimeout = setTimeout(() => {
           audioRef.current.pause();
           audioRef.current.currentTime = 0;
-          onTimerEnd();
+          onRoundFinished();
         }, 1500)
         return () => clearTimeout(audioPauseTimeout);
       }, (leftSeconds[currentTeam] - 1) * 1000)
---
+++ src/components/GameAndScoresTabs.jsx
@@ -0,0 +1,72 @@
+import {useDispatch, useSelector} from "react-redux";
+import React from "react";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import ScoresTab from "../components/ScoresTab";
+import Button from "@mui/material/Button";
+import GameTab from "../components/GameTab";
+import {setCurrentGameTab} from "../redux/gameSlice";
+
+const GameAndScoresTabs = () => {
+
+    const {roundInProgress, currentGameTab} = useSelector(state => state.game);
+    const dispatch = useDispatch();
+
+    const tabStyles = {
+        base: {
+            fontSize: '16px',
+            fontWeight: '600',
+            borderRadius: '100px'
+        },
+        active: {
+            backgroundColor: "#F0F0F0",
+            color: "#6B6B6B"
+        },
+        inactive: {
+            borderColor: '#D1D1D1',
+            backgroundColor: "#000000",
+            color: "#FFFFFF"
+        }
+    };
+
+    const handleTabChange = (tab) => {
+        if (!roundInProgress) {
+            dispatch(setCurrentGameTab(tab));
+        }
+    };
+
+    return (
+        <>
+            <ButtonGroup
+                sx={{
+                    height: '58px',
+                    opacity: roundInProgress ? '40%' : '100%',
+                    mt: 2,
+                    mb: 3
+                }}
+                fullWidth
+            >
+                <Button
+                    sx={{
+                        ...tabStyles.base,
+                        ...(currentGameTab === 'team' ? tabStyles.active : tabStyles.inactive)
+                    }}
+                    onClick={() => handleTabChange('team')}
+                >
+                    Команда
+                </Button>
+                <Button
+                    sx={{
+                        ...tabStyles.base,
+                        ...(currentGameTab === 'game' ? tabStyles.active : tabStyles.inactive)
+                    }}
+                    onClick={() => handleTabChange('game')}
+                >
+                    Игра
+                </Button>
+            </ButtonGroup>
+            {currentGameTab === 'game' && <GameTab/>}
+            {currentGameTab === 'team' && <ScoresTab/>}
+        </>
+    );
+};
+export default GameAndScoresTabs;
